bcbcd5404c028ea7e53f03992b883524786b7dfc
Filip Florczyk <filip.florczyk@n7space.com>
2025-08-07 13:45:58 +0200

    Draft implementation of Monitor activation log

---
+++ src/Monitor.c
@@ -0,0 +1,135 @@
+/**@file
+ * This file is part of the TASTE Leon3 Runtime.
+ *
+ * @copyright 2025 N7 Space Sp. z o.o.
+ *
+ * Leon3 Runtime is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Monitor.h>
+#include <Hal.h>
+
+#ifndef RT_EXEC_LOG_SIZE
+#define RT_EXEC_LOG_SIZE 0
+#endif
+
+#if RT_EXEC_LOG_SIZE <= 0
+static struct Monitor_InterfaceActivationEntry *_activation_log_buffer = NULL;
+#else
+static struct Monitor_InterfaceActivationEntry _activation_log_buffer[RT_EXEC_LOG_SIZE];
+#endif
+
+static bool _is_frozen = true;
+static uint32_t _latest_activation_entry_index = 0;
+static uint32_t _size_of_activation_log = 0;
+
+static void UpdateActivationLog(const uint32_t index, const enum interfaces_enum interface, 
+                                const enum Monitor_EntryType entry_type)
+{
+    uint64_t timestamp = Hal_GetElapsedTimeInNs();
+
+    _activation_log_buffer[_latest_activation_entry_index].interface = interface;
+    _activation_log_buffer[_latest_activation_entry_index].entry_type = entry_type;
+    _activation_log_buffer[_latest_activation_entry_index].timestamp = timestamp;
+}
+
+static bool HandleActivationLogCyclicInterface(const enum interfaces_enum interface, 
+                                               const enum Monitor_EntryType entry_Type)
+{
+    if(RT_EXEC_LOG_SIZE <= 0 || _is_frozen){
+        return false;
+    }
+
+    if(_latest_activation_entry_index == 0 && _size_of_activation_log == 0){
+        UpdateActivationLog(_latest_activation_entry_index, interface, entry_Type);
+        _size_of_activation_log++;
+    }
+    else if((_latest_activation_entry_index < RT_EXEC_LOG_SIZE - 1) && (_size_of_activation_log < RT_EXEC_LOG_SIZE)){
+        _latest_activation_entry_index++;
+        UpdateActivationLog(_latest_activation_entry_index, interface, entry_Type);
+        _size_of_activation_log++;
+    }
+    else if((_latest_activation_entry_index == RT_EXEC_LOG_SIZE - 1) && (_size_of_activation_log == RT_EXEC_LOG_SIZE)){
+        _latest_activation_entry_index = 0;
+        UpdateActivationLog(_latest_activation_entry_index, interface, entry_Type);
+    }
+    else if((_latest_activation_entry_index < RT_EXEC_LOG_SIZE - 1) && (_size_of_activation_log == RT_EXEC_LOG_SIZE)){
+        _latest_activation_entry_index++;
+        UpdateActivationLog(_latest_activation_entry_index, interface, entry_Type);
+    }
+    else{
+        return false;
+    }
+
+    return true;
+}
+
+bool Monitor_Init()
+{
+    return true;
+}
+
+bool Monitor_IndicateInterfaceActivated(const enum interfaces_enum interface)
+{
+    return HandleActivationLogCyclicInterface(interface, Monitor_EntryType_activation);
+}
+
+bool Monitor_IndicateInterfaceDeactivated(const enum interfaces_enum interface)
+{
+    return HandleActivationLogCyclicInterface(interface, Monitor_EntryType_deactivation);
+}
+
+bool Monitor_GetInterfaceActivationEntryLog(struct Monitor_InterfaceActivationEntry *activation_log, 
+                                            uint32_t *latest_activation_entry_index, 
+                                            uint32_t *size_of_activation_log)
+{
+    if(RT_EXEC_LOG_SIZE <= 0){
+        return false;
+    }
+
+    activation_log = _activation_log_buffer;
+    *latest_activation_entry_index = _latest_activation_entry_index;
+    *size_of_activation_log = _size_of_activation_log;
+
+    return true;
+}
+
+bool Monitor_FreezeInterfaceActivationLogging()
+{
+    if(RT_EXEC_LOG_SIZE <= 0){
+        return false;
+    }
+
+    _is_frozen = true;
+
+    return true;
+}
+
+bool Monitor_UnfreezeInterfaceActivationLogging()
+{
+    if(RT_EXEC_LOG_SIZE <= 0){
+        return false;
+    }
+
+    _is_frozen = false;
+
+    return true;
+}
+
+bool Monitor_ClearInterfaceActivationLog()
+{
+    if(RT_EXEC_LOG_SIZE <= 0){
+        return false;
+    }
+
+    _latest_activation_entry_index = 0;
+    _size_of_activation_log = 0;
+
+    return true;
+}
---
+++ src/Monitor.h
@@ -0,0 +1,107 @@
+/**@file
+ * This file is part of the TASTE Leon3 Runtime.
+ *
+ * @copyright 2025 N7 Space Sp. z o.o.
+ *
+ * Leon3 Runtime is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MONITOR_H
+#define MONITOR_H
+
+/**
+ * @file    Monitor.h
+ * @brief   Header for Monitor
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+enum Monitor_EntryType {
+    Monitor_EntryType_activation = 0,
+    Monitor_EntryType_deactivation = 1
+};
+
+struct Monitor_InterfaceActivationEntry {
+	enum interfaces_enum interface;
+	enum Monitor_EntryType entry_type;
+	uint64_t timestamp;
+};
+
+/**
+ * @brief                       Initializes the Monitor module.
+ *
+ * @return                      Bool indicating whether the initialization was
+ *                              successful
+ */
+bool Monitor_Init(void);
+
+/**
+ * @brief                       Informs the monitor about given interface activation, 
+ *                              monitor stores timestamp of activation in specific 
+ *                              configurable memory location.
+ *
+ * @param[in] interface         enum representing activated interface
+ * 
+ * @return                      Bool indicating whether the indication was
+ *                              successful
+ */
+bool Monitor_IndicateInterfaceActivated(const enum interfaces_enum interface);
+
+/**
+ * @brief                       Informs the monitor about given interface deactivation, 
+ *                              monitor stores timestamp of deactivation in specific 
+ *                              configurable memory location.
+ *
+ * @param[in] interface         enum representing deactivated interface
+ * 
+ * @return                      Bool indicating whether the indication was
+ *                              successful
+ */
+bool Monitor_IndicateInterfaceDeactivated(const enum interfaces_enum interface);
+
+/**
+ * @brief                       Provides access to optional interface activation log.
+ *
+ * @param[in] activation_log                    pointer pointing to beginning of cyclic buffer holding 
+ *                                              all activation entries
+ * @param[in] latest_activation_entry_index     representing latest activation log index
+ * @param[in] size_of_activation_log            representing size of activation log
+ * 
+ * @return                      Bool indicating whether the query was successful
+ */
+bool Monitor_GetInterfaceActivationEntryLog(struct Monitor_InterfaceActivationEntry *activation_log, 
+                                            uint32_t *latest_activation_entry_index, 
+                                            uint32_t *size_of_activation_log);
+
+/**
+ * @brief                       Stops storing of interface activation logs, all activation 
+ *                              logs are lost after this operation
+ * 
+ * @return                      Bool indicating whether the freeze was successful
+ */
+bool Monitor_FreezeInterfaceActivationLogging();
+
+/**
+ * @brief                       Starts storing of interface activation logs
+ * 
+ * @return                      Bool indicating whether the unfreeze was successful
+ */
+bool Monitor_UnfreezeInterfaceActivationLogging();
+
+/**
+ * @brief                       Clears all interface activation logs, resets latest_activation_entry_index 
+ *                              and size_of_activation_log
+ * 
+ * @return                      Bool indicating whether the clear was successful
+ */
+bool Monitor_ClearInterfaceActivationLog();
+
+#endif
