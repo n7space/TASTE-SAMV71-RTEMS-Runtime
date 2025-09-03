49193206c3be94cf7f5af88ccf7522e75a1f97bf
Filip Florczyk <filip.florczyk@n7space.com>
2025-09-03 10:48:41 +0200

    Review fixes

--- src/Hal.c
+++ src/Hal.c
@@ -1,15 +1,20 @@
 /**@file
- * This file is part of the TASTE Leon3 Runtime.
+ * This file is part of the TASTE SAMV71 RTEMS Runtime.
  *
  * @copyright 2025 N7 Space Sp. z o.o.
  *
- * Leon3 Runtime is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You should have received a copy of the GNU General Public License
- * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <Hal.h>
