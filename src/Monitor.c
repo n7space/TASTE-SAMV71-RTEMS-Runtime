562a32be79f2868174e3f7e7c657430f46f78575
Filip Florczyk <filip.florczyk@n7space.com>
2025-09-24 14:42:14 +0200

    Draft implementation of performance calculations

--- src/Monitor.c
+++ src/Monitor.c
@@ -1,15 +1,20 @@
 /**@file
- * This file is part of the TASTE Leon3 Runtime.
+ * This file is part of the TASTE SAMV71 RTEMS Runtime.
  *
  * @copyright 2025 N7 Space Sp. z o.o.
  *
- * Leon3 Runtime is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You should have received a copy of the GNU General Public License
- * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <Monitor.h>
@@ -28,6 +33,11 @@ static struct Monitor_InterfaceActivationEntry activation_log_buffer[RT_EXEC_LOG
 static bool is_frozen = true;
 static uint32_t latest_activation_entry_index = 0;
 static uint32_t size_of_activation_log = 0;
+
+static uint32_t benchmarking_ticks = 0;
+static int uptime_at_last_reset = 0;
+static struct Monitor_InterfaceUsageData usage_data;
+static struct Monitor_CPUUsageData idle_cpu_usage_data;
 
 static void UpdateActivationLog(const uint32_t index, const enum interfaces_enum interface, 
                                 const enum Monitor_EntryType entry_type)
@@ -70,8 +80,73 @@ static bool HandleActivationLogCyclicBuffer(const enum interfaces_enum interface
     return true;
 }
 
+static bool cpu_usage_visitor(Thread_Control *the_thread, void *arg)
+{
+    float usage_percent;
+	uint32_t integer_val;
+	uint32_t float_val;
+    int _Timestamp_Subtract
+	Timestamp_Control total_usage_time;
+	const Timestamp_Control used_time =
+	    _Thread_Get_CPU_time_used_after_last_reset(the_thread);
+
+	_TOD_Get_uptime(&uptime);
+	_Timestamp_Subtract(&uptime_at_last_reset, &uptime, &total_usage_time);
+	_Timestamp_Divide(&used_time, &total_usage_time, &integer_val, &float_val);
+
+	usage_percent = (float)float_val / TOD_NANOSECONDS_PER_MICROSECOND;
+	usage_percent += (float)integer_val;
+
+	if (usage_percent < idle_cpu_usage_data.minimum_cpu_usage) {
+		idle_cpu_usage_data.minimum_cpu_usage = usage_percent;
+	}
+
+	if (usage_percent < idle_cpu_usage_data.minimum_cpu_usage) {
+		idle_cpu_usage_data.minimum_cpu_usage = usage_percent;
+	}
+
+	idle_cpu_usage_data.average_cpu_usage =
+	    idle_cpu_usage_data.average_cpu_usage +
+	    (usage_percent - idle_cpu_usage_data.average_cpu_usage) /
+		(benchmarking_ticks + 1);
+
+    // only first idle thread is need, stop iteration after first step
+    return true;
+}
+
 bool Monitor_Init()
 {
+    _Timestamp_Set_to_zero(&total_usage_time);
+    rtems_cpu_usage_reset();
+	_TOD_Get_uptime(uptime_at_last_reset);
+	
+    for(int i = 0; i < RUNTIME_THREAD_COUNT; i++){
+        cpu_usage_data.maximum_cpu_usage = FLT_MAX;
+        cpu_usage_data.minimum_cpu_usage = 0.0;
+        cpu_usage_data.average_cpu_usage = 0.0;
+    }
+
+    return true;
+}
+
+bool Monitor_MonitoringTick(void)
+{
+	rtems_task_iterate(cpu_usage_visitor, NULL);
+    benchmarking_ticks++;
+}
+
+bool Monitor_GetUsageData(const enum interfaces_enum interface, Monitor_InterfaceUsageData *const usage_data)
+{
+    usage_data->interface = interface;
+    usage_data->maximum_execution_time = threads_info[interface].max_thread_execution_time;
+    usage_data->minimum_execution_time = threads_info[interface].min_thread_execution_time;
+    usage_data->average_execution_time = (uint64_t)threads_info[interface].mean_thread_execution_time;
+    return true;
+}
+
+bool Monitor_GetIdleCPUUsageData(Monitor_CPUUsageData *const cpu_usage_data)
+{
+    *cpu_usage_data = idle_cpu_usage_data;
     return true;
 }
 
