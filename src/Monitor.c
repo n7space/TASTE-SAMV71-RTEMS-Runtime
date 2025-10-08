6fbb5b9cdc24286d2608c899d04c7cd5843d6d82
Rafał Babski <rbabski@n7space.com>
2025-10-08 16:43:57 +0200

    Restored Monitor.{c,h} and re-formatted

--- src/Monitor.c
+++ src/Monitor.c
@@ -1,19 +1,24 @@
 /**@file
- * This file is part of the TASTE Leon3 Runtime.
+ * This file is part of the TASTE SAMV71 RTEMS Runtime.
  *
  * @copyright 2025 N7 Space Sp. z o.o.
  *
- * Leon3 Runtime is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You should have received a copy of the GNU General Public License
- * along with Leon3 Runtime. If not, see <http://www.gnu.org/licenses/>.
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <Hal.h>
 #include <Monitor.h>
+#include <Hal.h>
 
 #ifndef RT_EXEC_LOG_SIZE
 #define RT_EXEC_LOG_SIZE 0
@@ -26,78 +31,122 @@ static struct Monitor_InterfaceActivationEntry
 	activation_log_buffer[RT_EXEC_LOG_SIZE];
 #endif
 
-static bool is_frozen = true;
-static uint32_t latest_activation_entry_index = 0;
-static uint32_t size_of_activation_log = 0;
+static volatile bool is_frozen = true;
+static uint32_t activation_entry_counter = 0;
 
-static void UpdateActivationLog(const uint32_t index,
-				const enum interfaces_enum interface,
-				const enum Monitor_EntryType entry_type)
-{
-	const uint64_t timestamp = Hal_GetElapsedTimeInNs();
-
-	activation_log_buffer[latest_activation_entry_index].interface =
-		interface;
-	activation_log_buffer[latest_activation_entry_index].entry_type =
-		entry_type;
-	activation_log_buffer[latest_activation_entry_index].timestamp =
-		timestamp;
-}
+static uint32_t benchmarking_ticks = 0;
+static Timestamp_Control uptime_at_last_reset = 0;
+static Timestamp_Control total_usage_time = 0;
+static struct Monitor_CPUUsageData idle_cpu_usage_data;
 
 static bool
-HandleActivationLogCyclicBuffer(const enum interfaces_enum interface,
-				const enum Monitor_EntryType entry_Type)
+handle_activation_log_cyclic_buffer(const enum interfaces_enum interface,
+				    const enum Monitor_EntryType entry_type)
 {
 	if (RT_EXEC_LOG_SIZE <= 0 || is_frozen) {
 		return false;
 	}
 
-	if (latest_activation_entry_index == 0 && size_of_activation_log == 0) {
-		UpdateActivationLog(latest_activation_entry_index, interface,
-				    entry_Type);
-		size_of_activation_log++;
-	} else if ((latest_activation_entry_index < RT_EXEC_LOG_SIZE - 1) &&
-		   (size_of_activation_log < RT_EXEC_LOG_SIZE)) {
-		latest_activation_entry_index++;
-		UpdateActivationLog(latest_activation_entry_index, interface,
-				    entry_Type);
-		size_of_activation_log++;
-	} else if ((latest_activation_entry_index == RT_EXEC_LOG_SIZE - 1) &&
-		   (size_of_activation_log == RT_EXEC_LOG_SIZE)) {
-		latest_activation_entry_index = 0;
-		UpdateActivationLog(latest_activation_entry_index, interface,
-				    entry_Type);
-	} else if ((latest_activation_entry_index < RT_EXEC_LOG_SIZE - 1) &&
-		   (size_of_activation_log == RT_EXEC_LOG_SIZE)) {
-		latest_activation_entry_index++;
-		UpdateActivationLog(latest_activation_entry_index, interface,
-				    entry_Type);
-	} else {
-		return false;
+	activation_log_buffer[activation_entry_counter % RT_EXEC_LOG_SIZE]
+		.interface = interface;
+	activation_log_buffer[activation_entry_counter % RT_EXEC_LOG_SIZE]
+		.entry_type = entry_type;
+	activation_log_buffer[activation_entry_counter % RT_EXEC_LOG_SIZE]
+		.timestamp = Hal_GetElapsedTimeInNs();
+	activation_entry_counter++;
+
+	return true;
+}
+
+static bool cpu_usage_visitor(Thread_Control *the_thread, void *arg)
+{
+	float usage_percent;
+	uint32_t integer_val;
+	uint32_t float_val;
+	Timestamp_Control uptime;
+	const Timestamp_Control used_time =
+		_Thread_Get_CPU_time_used_after_last_reset(the_thread);
+
+	_TOD_Get_uptime(&uptime);
+	_Timestamp_Subtract(&uptime_at_last_reset, &uptime, &total_usage_time);
+	_Timestamp_Divide(&used_time, &total_usage_time, &integer_val,
+			  &float_val);
+
+	usage_percent = (float)float_val / TOD_NANOSECONDS_PER_MICROSECOND;
+	usage_percent += (float)integer_val;
+
+	if (usage_percent < idle_cpu_usage_data.minimum_cpu_usage) {
+		idle_cpu_usage_data.minimum_cpu_usage = usage_percent;
 	}
 
+	if (usage_percent > idle_cpu_usage_data.maximum_cpu_usage) {
+		idle_cpu_usage_data.maximum_cpu_usage = usage_percent;
+	}
+
+	idle_cpu_usage_data.average_cpu_usage =
+		idle_cpu_usage_data.average_cpu_usage +
+		(usage_percent - idle_cpu_usage_data.average_cpu_usage) /
+			(benchmarking_ticks + 1);
+
+	// only first idle thread is needed, stop iteration after first step
 	return true;
 }
 
 bool Monitor_Init()
 {
+	_Timestamp_Set_to_zero(&total_usage_time);
+	rtems_cpu_usage_reset();
+	_TOD_Get_uptime(&uptime_at_last_reset);
+
+	for (int i = 0; i < RUNTIME_THREAD_COUNT; i++) {
+		idle_cpu_usage_data.maximum_cpu_usage = 0.0;
+		idle_cpu_usage_data.minimum_cpu_usage = FLT_MAX;
+		idle_cpu_usage_data.average_cpu_usage = 0.0;
+	}
+
+	return true;
+}
+
+bool Monitor_MonitoringTick(void)
+{
+	rtems_task_iterate(cpu_usage_visitor, NULL);
+	benchmarking_ticks++;
+}
+
+bool Monitor_GetUsageData(const enum interfaces_enum interface,
+			  struct Monitor_InterfaceUsageData *const usage_data)
+{
+	usage_data->interface = interface;
+	usage_data->maximum_execution_time =
+		threads_info[interface].max_thread_execution_time;
+	usage_data->minimum_execution_time =
+		threads_info[interface].min_thread_execution_time;
+	usage_data->average_execution_time =
+		(uint64_t)threads_info[interface].mean_thread_execution_time;
+	return true;
+}
+
+bool Monitor_GetIdleCPUUsageData(
+	struct Monitor_CPUUsageData *const cpu_usage_data)
+{
+	*cpu_usage_data = idle_cpu_usage_data;
 	return true;
 }
 
 bool Monitor_IndicateInterfaceActivated(const enum interfaces_enum interface)
 {
-	return HandleActivationLogCyclicBuffer(interface,
-					       Monitor_EntryType_activation);
+	return handle_activation_log_cyclic_buffer(
+		interface, Monitor_EntryType_activation);
 }
 
 bool Monitor_IndicateInterfaceDeactivated(const enum interfaces_enum interface)
 {
-	return HandleActivationLogCyclicBuffer(interface,
-					       Monitor_EntryType_deactivation);
+	return handle_activation_log_cyclic_buffer(
+		interface, Monitor_EntryType_deactivation);
 }
 
 bool Monitor_GetInterfaceActivationEntryLog(
-	struct Monitor_InterfaceActivationEntry *activation_log,
+	struct Monitor_InterfaceActivationEntry **activation_log,
 	uint32_t *out_latest_activation_entry_index,
 	uint32_t *out_size_of_activation_log)
 {
@@ -105,9 +154,21 @@ bool Monitor_GetInterfaceActivationEntryLog(
 		return false;
 	}
 
-	activation_log = activation_log_buffer;
-	*out_latest_activation_entry_index = latest_activation_entry_index;
-	*out_size_of_activation_log = size_of_activation_log;
+	*activation_log = activation_log_buffer;
+
+	if (activation_entry_counter == 0) {
+		*out_latest_activation_entry_index = 0;
+		*out_size_of_activation_log = 0;
+	} else {
+		*out_latest_activation_entry_index =
+			(activation_entry_counter % RT_EXEC_LOG_SIZE) - 1;
+
+		if (activation_entry_counter > RT_EXEC_LOG_SIZE) {
+			*out_size_of_activation_log = RT_EXEC_LOG_SIZE;
+		} else {
+			*out_size_of_activation_log = activation_entry_counter;
+		}
+	}
 
 	return true;
 }
@@ -140,8 +201,7 @@ bool Monitor_ClearInterfaceActivationLog()
 		return false;
 	}
 
-	latest_activation_entry_index = 0;
-	size_of_activation_log = 0;
+	activation_entry_counter = 0;
 
 	return true;
 }
