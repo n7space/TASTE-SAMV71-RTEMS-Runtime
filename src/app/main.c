74a2e759c0a28df18203f9a8785c76280fe6dc6b
Rafał Babski <rbabski@n7space.com>
2025-10-16 14:32:39 +0200

    Added Hal_init in application

--- src/app/main.c
+++ src/app/main.c
@@ -1,4 +1,30 @@
+/**@file
+ * This file is part of the TASTE SAMV71 RTEMS Drivers.
+ *
+ * @copyright 2025 N7 Space Sp. z o.o.
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file     main.c
+ * @brief    File to check if drivers code can be properly compiled and linked.
+ */
+
 #include <rtems.h>
+
+#include <Hal.h>
 
 #define RUNTIME_TASK_COUNT (1 + 3 + 0)
 #define RUNTIME_FUNCTION_COUNT (1 + 2 + (0 * 2))
@@ -19,6 +45,7 @@
 
 rtems_task Init(rtems_task_argument argument)
 {
+	Hal_Init();
 }
 
 #define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
